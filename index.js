262fe2daa94f12b2506247abeeaf1643c35991bd
SmartinfoLogiks Tech Team <admin@smartinfologiks.com>
2021-05-05 19:40:08 +0000

    Check 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("First Commit");
+/*
+ * Main entry file to liveLogJS
+ *
+ *
+ * */
+
+var LIVELOGJS = {
+	
+	init: function() {
+		console.log("LiveLogJS Initialization");
+	}
+}
+
+module.exports = LIVELOGJS;
+
